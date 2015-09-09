6c27089ef403e6968ab8059ec76e8cc4e52dc461
BasuruK <www.basururoxyouall@gmail.com>
2015-09-09 21:25:54 +0530

    Case 2 -Incomplete
    74% Case 2 complete
    
    Case 2 minimum Time is not correct

--- Fixed Partition Manager/src/fixed/partition/manager/FixedPartitionManager.java
+++ Fixed Partition Manager/src/fixed/partition/manager/FixedPartitionManager.java
@@ -27,17 +27,15 @@ public class FixedPartitionManager {
         
         int [] memoryRegionsAndSizes = {3,5};
         int [] ram = {10,20,30};
-        int [][] programsAndTime = {{4,10,50,12,30,30,50,20,200}, {2,10,100,20,25}, {1,25,19}, {1,19,41}, {2,10,18,30,42}};
+        int [][] programsAndTime = {{2,10,50,12,30}, {2,10,100,20,25}, {1,25,19}, {1,19,41}, {2,10,18,30,42}};
         boolean [] isSet = {false, false, false};
         int timeCounter [][] = {{0, 0},{0, 0},{0, 0}};
         int memoryRegion;
         double avgTurnAroundTime = 0;
-        int minimumIndexValue = 0 ,minimumIndex = 0;
-        
-        
+        int minimumIndexValue = 0 ,minimumTime = 0;
+
         System.out.println(memoryRegionsAndSizes[0] + " " + memoryRegionsAndSizes[1]);
         System.out.println(ram[0] + " " + ram[1] + " " + ram[2]);
-        
         
             for(int i = 0; i < programsAndTime.length; i++)
             {
@@ -64,20 +62,23 @@ public class FixedPartitionManager {
             //Switch&Replace Sort
             if(programsAndTime[i][0] > 1)
             {
-                System.out.println( programsAndTime[i][0] + " Pairs on program " + (i+1));
+                minimumIndexValue = programsAndTime[i][2];
                 
                 for(int k = 1; k <= programsAndTime[i][0]*2;k++)
                 {
-                    minimumIndexValue = programsAndTime[i][2];
                     if((k%2)==0)
                     {
                         try
                         {
-                            System.out.println(k);
                             if(minimumIndexValue >= programsAndTime[i][k+2])
                             {
                                 minimumIndexValue = programsAndTime[i][k+2];
-                                minimumIndex = programsAndTime[i][k+1];
+                                minimumTime = programsAndTime[i][k+1];
+                            }
+                            else
+                            {
+                                minimumTime = programsAndTime[i][k-1];
+                                break;
                             }
                         }
                         catch(ArrayIndexOutOfBoundsException e)
@@ -86,95 +87,97 @@ public class FixedPartitionManager {
                         }
                     }
                 }
-                System.out.println("Minimum time is = " + minimumIndexValue + " ,Minimum Time's Size is =  " + minimumIndex + "\n");
+                programsAndTime[i][1] = minimumTime;
+                programsAndTime[i][2] = minimumIndexValue;
 
             }
-//            if(isSet[0]==false)
-//            {
-//                //Memory region 1
-//                if(programsAndTime[i][1] <= ram[0])
-//                {
-//                    memoryRegion = 1;
-//                    timeCounter[0][1]+=programsAndTime[i][2];
-//                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[0][0] + " to " + timeCounter[0][1] );
-//                    isSet[0]=true;
-//                    isSet[1]=false;
-//                    isSet[2]=false;
-//                    
-//                    timeCounter[0][0] = timeCounter[0][1];
-//                    avgTurnAroundTime+= timeCounter[0][1];
-//                }
-//                else if(programsAndTime[i][1] > ram[0] && programsAndTime[i][1] <= ram[1])
-//                {
-//                    memoryRegion = 2;
-//                    timeCounter[1][1]+=programsAndTime[i][2];
-//                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[1][0] + " to " + timeCounter[1][1] );
-//                    isSet[0]=true;
-//                    isSet[1]=false;
-//                    isSet[2]=false;
-//                   
-//                    timeCounter[1][0] = timeCounter[1][1];
-//                    avgTurnAroundTime += timeCounter[1][1];
-//                }
-//                else if(programsAndTime[i][1] > ram[1] && programsAndTime[i][1] <= ram[2])
-//                {
-//                    memoryRegion = 3;
-//                    timeCounter[2][1]+=programsAndTime[i][2];
-//                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[2][0] + " to " + timeCounter[2][1] );
-//                    isSet[0]=true;
-//                    isSet[1]=false;
-//                    isSet[2]=false;
-//                    
-//                    timeCounter[2][0] = timeCounter[2][1];
-//                    avgTurnAroundTime += timeCounter[2][1];
-//                    
-//                }
-//            }
-//            else if(isSet[1]==false)
-//            {
-//                //Memory region 2
-//                if(programsAndTime[i][1] <= ram[1])
-//                {
-//                    memoryRegion = 2;
-//                    timeCounter[1][1]+=programsAndTime[i][2];
-//                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[1][0] + " to " + timeCounter[1][1] );
-//                    isSet[1]=true;
-//                    isSet[0]=false;
-//                    isSet[2]=false;
-//                    
-//                    timeCounter[1][0] = timeCounter[1][1];
-//                    avgTurnAroundTime+= timeCounter[1][1];
-//                }
-//                else if(programsAndTime[i][1] > ram[1] && programsAndTime[i][1] <= ram[2])
-//                {
-//                    memoryRegion = 3;
-//                    timeCounter[2][1]+=programsAndTime[i][2];
-//                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[2][0] + " to " + timeCounter[2][1] );
-//                    isSet[1]=true;
-//                    isSet[0]=false;
-//                    isSet[2]=false;
-//                    
-//                    timeCounter[2][0] = timeCounter[2][1];
-//                    avgTurnAroundTime+= timeCounter[2][1];
-//                }
-//            }
-//            else if(isSet[2]==false)
-//            {
-//                //Memory region 3
-//                if(programsAndTime[i][1] <= ram[2])
-//                {
-//                    System.out.println("sdsds");
-//                    memoryRegion = 3;
-//                    timeCounter[2][1]+=programsAndTime[i][2];
-//                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[2][0] + " to " + timeCounter[2][1] );
-//                    isSet[0]=false;
-//                    isSet[1]=false;
-//                    isSet[2]=true;
-//                    timeCounter[2][0] = timeCounter[2][1];
-//                    avgTurnAroundTime += timeCounter[2][1];
-//                }
-//            }
-//            
+            //Assiginin Start
+            if(isSet[0]==false)
+            {
+                //Memory region 1
+                if(programsAndTime[i][1] <= ram[0])
+                {
+                    memoryRegion = 1;
+                    timeCounter[0][1]+=programsAndTime[i][2];
+                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[0][0] + " to " + timeCounter[0][1] );
+                    isSet[0]=true;
+                    isSet[1]=false;
+                    isSet[2]=false;
+                    
+                    timeCounter[0][0] = timeCounter[0][1];
+                    avgTurnAroundTime+= timeCounter[0][1];
+                }
+                else if(programsAndTime[i][1] > ram[0] && programsAndTime[i][1] <= ram[1])
+                {
+                    memoryRegion = 2;
+                    timeCounter[1][1]+=programsAndTime[i][2];
+                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[1][0] + " to " + timeCounter[1][1] );
+                    isSet[0]=true;
+                    isSet[1]=false;
+                    isSet[2]=false;
+                   
+                    timeCounter[1][0] = timeCounter[1][1];
+                    avgTurnAroundTime += timeCounter[1][1];
+                }
+                else if(programsAndTime[i][1] > ram[1] && programsAndTime[i][1] <= ram[2])
+                {
+                    memoryRegion = 3;
+                    timeCounter[2][1]+=programsAndTime[i][2];
+                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[2][0] + " to " + timeCounter[2][1] );
+                    isSet[0]=true;
+                    isSet[1]=false;
+                    isSet[2]=false;
+                    
+                    timeCounter[2][0] = timeCounter[2][1];
+                    avgTurnAroundTime += timeCounter[2][1];
+                    
+                }
+            }
+            else if(isSet[1]==false)
+            {
+                //Memory region 2
+                if(programsAndTime[i][1] <= ram[1])
+                {
+                    memoryRegion = 2;
+                    timeCounter[1][1]+=programsAndTime[i][2];
+                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[1][0] + " to " + timeCounter[1][1] );
+                    isSet[1]=true;
+                    isSet[0]=false;
+                    isSet[2]=false;
+                    
+                    timeCounter[1][0] = timeCounter[1][1];
+                    avgTurnAroundTime+= timeCounter[1][1];
+                }
+                else if(programsAndTime[i][1] > ram[1] && programsAndTime[i][1] <= ram[2])
+                {
+                    memoryRegion = 3;
+                    timeCounter[2][1]+=programsAndTime[i][2];
+                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[2][0] + " to " + timeCounter[2][1] );
+                    isSet[1]=true;
+                    isSet[0]=false;
+                    isSet[2]=false;
+                    
+                    timeCounter[2][0] = timeCounter[2][1];
+                    avgTurnAroundTime+= timeCounter[2][1];
+                }
+            }
+            else if(isSet[2]==false)
+            {
+                //Memory region 3
+                if(programsAndTime[i][1] <= ram[2])
+                {
+                    System.out.println("sdsds");
+                    memoryRegion = 3;
+                    timeCounter[2][1]+=programsAndTime[i][2];
+                    System.out.println("Program "+ (i+1) + " runs in region " + memoryRegion + " from " + timeCounter[2][0] + " to " + timeCounter[2][1] );
+                    isSet[0]=false;
+                    isSet[1]=false;
+                    isSet[2]=true;
+                    timeCounter[2][0] = timeCounter[2][1];
+                    avgTurnAroundTime += timeCounter[2][1];
+                }
+            }
+            
         }
         System.out.println("Average turnaround time = " + avgTurnAroundTime/memoryRegionsAndSizes[1]);
     }
